acc60b234bea2ac8d0ba823881dfa66a92a5f077
Antony404 <antony418@icloud.com>
2023-12-12 16:23:56 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "com.ant00000ny"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(8)
+}
---
+++ src/main/kotlin/DefaultObjectMapper.kt
@@ -0,0 +1,7 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import okhttp3.OkHttpClient
+
+val objectMapper: ObjectMapper = ObjectMapper()
+val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+    
+    .build()
